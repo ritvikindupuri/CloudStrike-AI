0539edb422303f0f0fe90163bbe67f752507f1ee
Ritvik Indupuri <Ritvik.Indupuri@gmail.com>
2025-06-27 14:53:39 +0000

    do you tjink we should change the app name or logo or no does it perfect

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -9,13 +9,22 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      strokeWidth="2"
+      strokeWidth="1.5"
       strokeLinecap="round"
       strokeLinejoin="round"
       className="h-6 w-6 text-primary"
       {...props}
     >
-      <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z" />
+      {/* Shield Outline */}
+      <path d="M12 2L3 5v6c0 5.6 3.8 10.7 9 12 5.2-1.3 9-6.4 9-12V5l-9-3z" />
+      {/* Inner Circuit */}
+      <path d="M12 12m-1.5 0a1.5 1.5 0 1 0 3 0a1.5 1.5 0 1 0 -3 0" />
+      <path d="M12 10.5V9" />
+      <path d="M12 15v-1.5" />
+      <path d="M14.5 12.5l1.5-1" />
+      <path d="M8 11.5l1.5 1" />
+      <path d="M9.5 14.5l-1.5 1" />
+      <path d="M16 13.5l-1.5-1" />
     </svg>
   );
 }
