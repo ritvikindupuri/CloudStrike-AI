673657922626a822ee522b9301e994dfb155b825
Ritvik Indupuri <Ritvik.Indupuri@gmail.com>
2025-06-27 15:13:13 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -17,13 +17,6 @@ export function Logo({ className, ...props }: SVGProps<SVGSVGElement>) {
       {...props}
     >
       <path d="M12 2L3 5v6c0 5.6 3.8 10.7 9 12 5.2-1.3 9-6.4 9-12V5l-9-3z" />
-      <circle cx="12" cy="12" r="1.5" />
-      <path d="M12 10.5V9" />
-      <path d="M12 15v-1.5" />
-      <path d="M14.5 12.5l1.5-1" />
-      <path d="M8 11.5l1.5 1" />
-      <path d="M9.5 14.5l-1.5 1" />
-      <path d="M16 13.5l-1.5-1" />
     </svg>
   );
 }
