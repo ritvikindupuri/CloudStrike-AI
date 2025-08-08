7634224a34779f1c38e593b98e71a127c4ba9137
Ritvik Indupuri <Ritvik.Indupuri@gmail.com>
2025-08-08 18:13:02 +0000

    for Cloudstrike the logo looks very messy i dont think it rendred a clou

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -14,13 +14,7 @@ export function Logo({ className, ...props }: SVGProps<SVGSVGElement>) {
       className={cn("h-6 w-6 text-primary", className)}
       {...props}
     >
-      <path d="m5 8 6 6"></path>
-      <path d="m4 14 6-6 2-3"></path>
-      <path d="M2 5h12"></path>
-      <path d="M7 2h1"></path>
-      <path d="m12 5 2 3 6 6"></path>
-      <path d="m8 14 4 4 6-6"></path>
-      <path d="M11 4h10"></path>
+        <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
     </svg>
   );
 }
